b9b9bb1f2b022c6cdb9ee43a6e237b5ef4ada265
AddamsonWilliams1123 <hellsec0x0@mailfence.com>
2024-07-20 11:02:38 +1200

    Add files via upload

---
+++ about-us/about-us.php
@@ -0,0 +1,51 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <title>💀HellSec💀 - About US</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="ab.css">
+    </head>
+
+    <body>
+        <img class="anon" src="/img/Untitled-removebg-preview(1).png">
+        <img class="logo" src="/img/logo.png">
+        <div>
+            <a class="button1" href="/index.php">Home</a>
+            <a class="button2" href="/about-us/about-us.php">About Us</a>
+            <a class="button3"  href="/leaks/leaks.php">Leaks</a>
+            <a class="button4"  href="/contact-us/contact-us.php">Contact Us</a>
+        </div>
+        <div class="content">
+            <p class="text1"><b>HellSec</b> is a hacking group that fights against internet censorship, child pornography, </p>
+            <p class="text2">violence and any other injustices that may come our way.</p>
+            <p class="text3">We invite you to join the fight.</p>
+            <p class="text4">Some members of our collective are former or current members of the hacker collective Anonymous.</p>
+            <p class="text5">We work together, we have one goal.</p>
+            <p class="text6">When we connect and fight as one, nothing can stand in our way.</p>
+        </div>
+
+        <h1 class="texte1"><b>Report Crime:</b></h1>
+        <div class="content2">
+            <div class="texte2">
+                <p>If you're in danger, call: 911, 112 or 000</p>
+                <p>Global</p>
+                <p>NCMEC’s CyberTipline:<a href="https://www.missingkids.org/gethelpnow/cybertipline">Website</a></p>
+                <p>Defence for Children:<a href="https://dontlookaway.report/">Website</a></p>
+                <p>United States</p>
+                <p>FBI:<a href="https://tips.fbi.gov/home">Website</a></p>
+                <p>FBI Kidnappings & Missing Persons:<a href="https://www.fbi.gov/wanted/kidnap">Website</a></p>
+                <p>CIA:<a href="https://www.cia.gov/">Website</a></p>
+                <p>Justice:<a href="https://www.justice.gov/">Website</a></p>
+                <p>IC3:<a href="https://www.ic3.gov/Home/ComplaintChoice">Website</a></p>
+                <p>DEA:<a href="https://www.dea.gov/submit-tip">Website</a></p>
+                <p>ICE:<a href="https://www.ice.gov/webform/ice-tip-form">Website</a></p>
+                <p>Europe</p>
+                <p>INTERPOL:<a href="https://www.interpol.int/Contacts/Contact-INTERPOL">Website</a></p>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ leaks/leaks.php
@@ -0,0 +1,67 @@
+<?php
+
+?>
+
+<html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>💀HellSec💀 - Leaks</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="lk.css">
+    </head>
+
+    <body>
+        <div>
+            <a  class="button1" href="/index.php">Home</a>
+            <a  class="button2" href="/about-us/about-us.php">About Us</a>
+            <a  class="button3" href="/leaks/leaks.php">Leaks</a>
+            <a  class="button4" href="/contact-us/contact-us.php">Contact Us</a>
+        </div>
+
+        <div class="post1">     
+        </div>
+        <div class="post2">     
+        </div>
+        <div class="post3">     
+        </div>
+        <div class="post4">     
+        </div>
+
+        <div>
+            <h1 class="text1">
+                Soon...
+            </h1>
+
+            <p class="text2">
+            💀 For more things check our Twitter 💀
+            </p>
+        </div>
+        <div>
+            <h1 class="text3">
+                Soon...
+            </h1>
+
+            <p class="text4">
+            💀 For more things check our Twitter 💀
+            </p>
+        </div>
+        <div>
+            <h1 class="text5">
+                Soon...
+            </h1>
+
+            <p class="text6">
+            💀 For more things check our Twitter 💀
+            </p>
+        </div>
+        <div>
+            <h1 class="text7">
+                Soon...
+            </h1>
+
+            <p class="text8">
+            💀 For more things check our Twitter 💀
+            </p>
+        </div>
+    </body>
+</html>
